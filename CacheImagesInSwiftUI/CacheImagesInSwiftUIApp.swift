093b086f6bde09825849a46ccc6a80656bb64879
Uliana Gritchina <ugritchina@icloud.com>
2022-07-11 16:27:33 +0300

    Initial Commit

---
+++ CacheImagesInSwiftUI/CacheImagesInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CacheImagesInSwiftUIApp.swift
+//  CacheImagesInSwiftUI
+//
+//  Created by Ульяна Гритчина on 11.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct CacheImagesInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
